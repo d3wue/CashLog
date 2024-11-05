2c541f87fd0939537f91b3cc982b868c99ce5cd4
Nikolai Stein <nikolai.stein86@gmail.com>
2024-11-05 09:42:40 +0100

    update

--- basicModel.py
+++ basicModel.py
@@ -6,17 +6,17 @@ from pulp import *
 class CashLogWLP:
     # MIP for the CashLog warehouse location problem
     def __init__(self):
-        self.warehouses = pd.read_csv('https://raw.githubusercontent.com/NikoStein/CashLog/master/data/warehouses.csv', index_col='warehouseID')
+        self.warehouses = pd.read_csv('data/warehouses.csv', index_col='warehouseID')
         self.W = self.warehouses.index.values
-        self.regions = pd.read_csv('https://raw.githubusercontent.com/NikoStein/CashLog/master/data/regions.csv', index_col='regionID')
+        self.regions = pd.read_csv('data/regions.csv', index_col='regionID')
         self.R = self.regions.index.values
-        self.shifts = pd.read_csv('https://raw.githubusercontent.com/NikoStein/CashLog/master/data/shifts.csv', index_col=['warehouseID', 'regionID'])
+        self.shifts = pd.read_csv('data/shifts.csv', index_col=['warehouseID', 'regionID'])
         self.S = self.shifts.index.values
                 
     def solve(self, n_warehouses=-1, force_open = []):
         prob = LpProblem('CashLog_BasicAnalysis', LpMinimize)
-        x = LpVariable.dicts(name='x', indices=self.S, cat=LpBinary)
-        y = LpVariable.dicts(name='y', indices=self.W, cat=LpBinary)
+        x = LpVariable.dicts(name='x', indexs=self.S, cat=LpBinary)
+        y = LpVariable.dicts(name='y', indexs=self.W, cat=LpBinary)
         
         fixedCosts = lpSum([y[w] * self.warehouses.loc[w].fixedCosts for w in self.W])
         variableCosts = lpSum([x[w,r] * self.shifts.loc[w,r].transportationCosts for w,r in self.S]) 
@@ -36,7 +36,7 @@ class CashLogWLP:
         if n_warehouses != -1:
             prob += lpSum([y[w] for w in self.W]) == n_warehouses
 
-        status = prob.solve()
+        status = prob.solve(PULP_CBC_CMD(msg=0))
         
         self.totalCosts = prob.objective.value()
         self.fixedCosts = sum([y[w].varValue * self.warehouses.loc[w].fixedCosts for w in self.W]) 
